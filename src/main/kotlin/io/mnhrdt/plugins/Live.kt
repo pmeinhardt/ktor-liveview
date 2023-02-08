11f2e3a02335314344eeda6b54d692f697fdb592
pmeinhardt <mnhrdt@gmail.com>
2023-02-08 23:48:56 +0100

    Sketch out an initial, simplified LiveView API

---
+++ src/main/kotlin/io/mnhrdt/plugins/Live.kt
@@ -0,0 +1,44 @@
+package io.mnhrdt.plugins
+
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.html.*
+import kotlinx.html.stream.*
+
+class LiveViewContext(val parameters: Parameters)
+
+abstract class LiveView {
+    abstract fun render(): String
+}
+
+fun LiveView.html(block: HTML.() -> Unit): String =
+    buildString { append("<!DOCTYPE html>\n").appendHTML().html(block = block) }
+
+fun Route.live(path: String, init: LiveViewContext.() -> LiveView) {
+    get(path) {
+        val context = LiveViewContext(call.parameters)
+        val view = init(context)
+
+        val content = view.render()
+
+        val type = ContentType.Text.Html.withCharset(Charsets.UTF_8)
+        val ok = HttpStatusCode.OK
+
+        call.respond(TextContent(content, type, ok))
+    }
+
+    webSocket(path) {
+        val context = LiveViewContext(call.parameters)
+        val view = init(context)
+
+        for (msg in incoming) {
+            val content = view.render()
+            send(Frame.Text(content))
+        }
+    }
+}
--- src/main/kotlin/io/mnhrdt/plugins/Routing.kt
+++ src/main/kotlin/io/mnhrdt/plugins/Routing.kt
@@ -3,10 +3,26 @@ package io.mnhrdt.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.routing.*
+import java.util.Date
+import kotlinx.html.*
+
+class Index(private val name: String) : LiveView() {
+    override fun render(): String =
+        html {
+            head { title { +"Hello $name" } }
+            body { p { +"It is ${now()}" } }
+        }
+
+    private fun now() = Date()
+}
 
 fun Application.configureRouting() {
     install(AutoHeadResponse)
 
     routing {
+        live("/") {
+            val name = parameters["name"] ?: "Ktor"
+            Index(name)
+        }
     }
 }
